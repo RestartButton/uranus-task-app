09f2bda41b44a7b4e13e314460eb118466796df6
Rogério Franchini Borges Júnior <rogeriofbjr@hotmail.com>
2025-02-20 13:16:37 -0300

    Tentando adicionar Auth

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,5 +1,7 @@
 public class User
 {
     public int Id { get; set; }
-    public required string Nome { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
 }
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,25 @@ builder.Services.AddDbContext<ApplicationDbContext>(
         sqlServerOptions => sqlServerOptions.EnableRetryOnFailure()
     )
 );
+builder.Services.AddScoped<AuthService>();
+
+var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => 
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddCors(options =>
 {
@@ -23,6 +45,7 @@ var app = builder.Build();
 
 app.UseCors("AllowFrontend");
 
+app.UseAuthentication();
 app.UseAuthorization();
 // Middlewares
 app.MapControllers();
